38cb878b9d4880cdb3a1ff8680730490c9d16c39
Zack Worden <zackw@technologytherapy.com>
2019-05-20 14:55:59 -0400

    Roughed out blog archive
    added home.php template (tu - feel free to split this however you need to)
    custom sidebar for blog / post archive
    content-post for... post... content.

---
+++ wp-content/themes/tastedesigns/sidebar-blogArchive.php
@@ -0,0 +1,18 @@
+<aside id="sidebar sidebar-blogArchive" class="widget-area">
+	<?php
+
+
+	?>
+	<div style="margin: 2rem 0; padding: 1rem 0;">
+		sidebar block
+	</div>
+	<div style="margin: 2rem 0; padding: 1rem 0;">
+		sidebar block
+	</div>
+	<div style="margin: 2rem 0; padding: 1rem 0;">
+		sidebar block
+	</div>
+	<div style="margin: 2rem 0; padding: 1rem 0;">
+		sidebar block
+	</div>
+</aside>
