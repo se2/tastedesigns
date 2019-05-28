c09fbcb06ff7fc742b4c1c9a5a2de9eafb8b2b92
Hoa Nguyen <luongduc.tu1992@gmail.com>
2019-05-28 14:05:44 +0700

    Blog Listing

--- wp-content/themes/tastedesigns/home.php
+++ wp-content/themes/tastedesigns/home.php
@@ -14,32 +14,44 @@ get_header();
 	<h1 class="c-header__title font-title text-center text-taste-1"><?php echo get_the_title(get_option('page_for_posts', true)); ?></h1>
 </div>
 
-<div class="c-archive">
-	<?php if ( have_posts() ) : ?>
+<div class="blog-content flex flex-wrap relative">
 
-		<div class="c-archive__content flex flex-wrap">
+	<div class="c-archive" style="order: 1;">
+		<?php if ( have_posts() ) : ?>
+
+			<div class="c-archive__content flex flex-wrap">
+				<?php
+
+				while ( have_posts() ) :
+					the_post();
+
+					get_template_part( 'template-parts/content', get_post_type() );
+
+				endwhile;
+				?>
+			</div>
+
+			<a href="//localhost:3000" class="c-button__link no-underline">
+				<div class="c-button border-2 border-taste-4 px-21 inline-flex items-center">
+					<span class="c-button__text uppercase text-14 text-taste-2 font-subtitle">View more Posts</span>
+					<img src="//localhost:3000/wp-content/themes/tastedesigns/img/alternative-path.png" alt="Enter" class="c-button__icon h-auto ml-auto lg:ml-30">
+				</div>
+			</a>
+
 			<?php
 
-			while ( have_posts() ) :
-				the_post();
 
-				get_template_part( 'template-parts/content', get_post_type() );
+		else :
 
-			endwhile;
-			?>
-		</div>
+			get_template_part( 'template-parts/content', 'none' );
 
-		<?php
-		the_posts_navigation();
+		endif;
+		?>
+	</div>
 
-	else :
+	<?php get_sidebar('blogs'); ?>
 
-		get_template_part( 'template-parts/content', 'none' );
-
-	endif;
-	?>
 </div>
 
 <?php
-get_sidebar('blogs');
 get_footer();
--- wp-content/themes/tastedesigns/sidebar-blogs.php
+++ wp-content/themes/tastedesigns/sidebar-blogs.php
@@ -1,4 +1,5 @@
-<aside id="sidebar sidebar-blogs" class="widget-area c-sidebar-blogs">
+<!-- Desktop -->
+<aside id="sidebar sidebar-blogs" class="px-20 widget-area c-sidebar-blogs c-sidebar-blogs__desktop">
 	<h2 class="c-sidebar-blogs__title">
 		Sort by Category:
 	</h2>
@@ -7,5 +8,15 @@
 		Search the Blog:
 	</h2>
 	<?php echo do_shortcode('[facetwp facet="blogs_search"]'); ?>
+	<?php do_action('ttg_blogsCta'); ?>
+</aside>
+
+<!-- Mobile -->
+<aside id="sidebar sidebar-blogs" class="widget-area c-sidebar-blogs c-sidebar-blogs__mobile px-0 lg:px-20" style="order: 0;">
+	<?php echo do_shortcode('[facetwp facet="categories"]'); ?>
+	<?php echo do_shortcode('[facetwp facet="blogs_search"]'); ?>
+</aside>
+
+<aside id="sidebar sidebar-blogs" class="px-20 widget-area c-sidebar-blogs c-sidebar-blogs__mobile" style="order: 2;">
 	<?php do_action('ttg_blogsCta'); ?>
 </aside>
