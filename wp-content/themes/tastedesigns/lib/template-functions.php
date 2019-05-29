42cd193a43d2f4d44d400a2207e66bf38925ae7c
Tu Luong <luongduc.tu1992@gmail.com>
2019-05-29 13:10:55 +0700

    Temporarily disable srcset attr

--- wp-content/themes/tastedesigns/lib/template-functions.php
+++ wp-content/themes/tastedesigns/lib/template-functions.php
@@ -56,3 +56,10 @@ function my_layout_title( $title, $field, $layout, $i ) {
 }
 
 add_filter( 'acf/fields/flexible_content/layout_title', 'my_layout_title', 10, 4 );
+
+// disable srcset on frontend
+function disable_wp_responsive_images() {
+	return 1;
+}
+
+add_filter('max_srcset_image_width', 'disable_wp_responsive_images');
