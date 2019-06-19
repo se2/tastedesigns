161f1b7f136b36d6c1aea99cab74a3cfbfd1fffd
Tu Luong <luongduc.tu1992@gmail.com>
2019-06-19 16:42:31 +0700

    Gallery At Taste

--- wp-content/themes/tastedesigns/templates/taste-gallery.php
+++ wp-content/themes/tastedesigns/templates/taste-gallery.php
@@ -76,23 +76,18 @@ if (have_posts()) : ?>
 					/**
 					 * Artists
 					 */
-
-					$args = array (
-						'post_type' => 'artist',
-						'posts_per_page' => -1,
-					);
-					$query = new WP_Query($args);
 					?>
 
-					<?php if ($query->have_posts()) : ?>
+					<?php if (have_rows('artists')) : ?>
 						<div class="c-artists w-full py-60 lg:py-100">
 							<h1 class="font-title text-40 leading-50/40 lg:text-66 lg:leading-89 text-taste-1 text-center capitalize" <?php $colors->getPrimary(); ?>>
 								Artists
 							</h1>
 							<div class="c-artists__content px-0 lg:px-10 flex flex-wrap mt-40 lg:mt-60">
-								<?php while ($query->have_posts()) : $query->the_post(); ?>
+								<?php while (have_rows('artists')) : the_row(); ?>
+									<?php $artist = get_sub_field('artist'); ?>
 									<div class="c-artists__item w-full lg:w-1/3 px-0 lg:px-6 lg:py-24">
-										<a href="<?php the_permalink(); ?>" class="no-underline">
+										<a href="<?php echo get_permalink($artist); ?>" class="no-underline">
 											<div class="c-artists__item-inner px-20 py-20 lg:px-15 lg:py-16 hover:bg-taste-5"
 											<?php if ($colors->isOverriden()) : ?>
 												onMouseOver="this.style.backgroundColor='<?php echo $colors->getRawTertiary(); ?>'"
@@ -100,12 +95,12 @@ if (have_posts()) : ?>
 											<?php endif; ?>
 											>
 												<div class="c-artists__thumbnail w-full h-team-image-mobile lg:h-artist-image bg-taste-9">
-													<?php the_post_thumbnail('small', [
+													<?php echo get_the_post_thumbnail($artist, 'small', [
 														'class' => 'c-artists__image w-full h-full o-cover o-top block'
 													]); ?>
 												</div>
 												<h2 class="font-body text-16 leading-30.16 tracking-3.71 text-center text-taste-2 no-underline uppercase mt-20" <?php $colors->getPrimary(); ?>>
-													<?php the_title(); ?>
+													<?php echo get_the_title($artist); ?>
 												</h2>
 											</div>
 										</a>
