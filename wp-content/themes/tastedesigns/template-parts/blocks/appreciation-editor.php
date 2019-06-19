03e0290a7dc1ae35a5086b4071bf6e7a70029ba1
Tu Luong <luongduc.tu1992@gmail.com>
2019-06-20 01:37:12 +0700

    Page Blocks

--- wp-content/themes/tastedesigns/template-parts/blocks/appreciation-editor.php
+++ wp-content/themes/tastedesigns/template-parts/blocks/appreciation-editor.php
@@ -19,7 +19,7 @@ if ($testimonial) :
 ?>
 
   <section class="c-appreciation w-full relative flex flex-col lg:flex-row">
-    <div class="c-appreciation__content py-70 px-0 lg:pl-50 lg:pr-50">
+    <div class="c-appreciation__content py-70 px-0 lg:pl-50 lg:pr-50 flex-1">
       <h2 class="font-title text-30 leading-40 text-center lg:text-left lg:text-42 lg:leading-57 text-taste-4" <?php $colors->getSecondary(); ?>>
         Appreciation
       </h2>
@@ -49,8 +49,8 @@ if ($testimonial) :
         <?php endif; ?>
       </div>
     </div>
-    <div class="c-appreciation__image-container lg:ml-50 w-full h-appreciation-mobile lg:h-auto lg:w-appreciation overflow-hidden">
-      <?php generate_image(get_post_thumbnail_id(get_the_ID()), 'c-appreciation__image h-full w-full', 'medium'); ?>
+    <div class="c-appreciation__image-container lg:ml-50 w-full h-appreciation-mobile lg:h-auto lg:w-appreciation overflow-hidden relative">
+      <?php generate_image(get_post_thumbnail_id(get_the_ID()), 'c-appreciation__image h-full w-full o-cover absolute top-0 left-0', 'medium'); ?>
     </div>
   </section>
 
